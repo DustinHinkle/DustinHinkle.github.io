dd0b46da82159f28182eaa65e86a56e312facfbd
Dustin Hinkle <Dustin.Hinkle.R@gmail.com>
2023-03-20 16:03:52 -0700

    commit

---
+++ calculations.js
@@ -0,0 +1,7 @@
+function average()
+{
+    var one = document.getElementById("highOne");
+    var two = document.getElementById("highTwo");
+    var three = document.getElementById("highThree");
+    Average = one+two+three / 3;
+}
