490c4195e61251ab3879259f80b347421501f535
Dustin Hinkle <Dustin.Hinkle.R@gmail.com>
2023-03-21 15:15:33 -0700

    Functional

--- calculations.js
+++ calculations.js
@@ -1,12 +1,46 @@
-function average()
-{
-    var one = document.getElementById("highOne");
-    var two = document.getElementById("highTwo");
-    var three = document.getElementById("highThree");
-    threeAverage = one+two+three / 3;
-    document.getElementById("average").innerHTML = threeAverage;
-}
 function go()
 {
-    average();
+    var one = parseFloat(document.getElementById("highOne").value);
+    var two = parseFloat(document.getElementById("highTwo").value);
+    var three = parseFloat(document.getElementById("highThree").value);
+    
+    var threeAverage = (one+two+three)/3;
+
+    var yos = parseInt(document.getElementById("yos").value);
+    var age = parseInt(document.getElementById("age").value);
+    var months = parseInt(document.getElementById("months").value);
+    
+
+    if (yos >= 20 && age >= 62)
+    {
+        //1.1%
+        var rating = "1.1% ";
+        var rate = (threeAverage*.011) * yos;
+    }else if (age < 62 || (age >= 62 && yos < 20))
+    {
+        var rating = "1.0% ";
+        var rate = (threeAverage*.01) * yos;
+    }
+    
+    var reductionRating = months * (5/12);
+
+    if (reductionRating == 0)
+    {
+        var annuity = rate;
+    }else if (reductionRating > 0)
+    {
+        var reduction = reductionRating / 100;
+        if(yos >= 30)
+        {
+            var reduction = 0;
+            var reductionRating = 0;
+        }
+        var annuity = rate - (rate * reduction);
+    }
+    document.getElementById("hidden").style.display = "block";
+
+    document.getElementById("average").innerHTML = "Your three year high average is: " + threeAverage.toFixed(2);
+    document.getElementById("rate").innerHTML = "Your rate is " + rating + "for each year of service, which comes to a annuity of: " + rate.toFixed(2);
+    document.getElementById("reduction").innerHTML = "Your reduction is: " + reductionRating + "%";
+    document.getElementById("final").innerHTML = "Your final estimated annuity is: " + annuity.toFixed(2);
 }
